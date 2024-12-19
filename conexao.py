af2c6cf49aa3330b3394c04ee1ac29006de77232
thomasluizon <thomaslrgregorio@gmail.com>
2024-12-18 21:20:04 -0300

    init

---
+++ conexao.py
@@ -0,0 +1,8 @@
+import pymongo
+
+
+class ConexaoClass:
+    def conexaoMongoDB(self, url, banco):
+        conexao = pymongo.MongoClient(url)
+        bancodados = conexao[banco]
+        return bancodados
